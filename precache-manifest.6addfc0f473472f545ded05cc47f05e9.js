3f142996ad2b38c1f2ffdb4717b07920719d5fc5
Douglas <Douglas_0608@hotmail.com>
2020-06-16 10:50:54 -0300

    Updates

---
+++ precache-manifest.6addfc0f473472f545ded05cc47f05e9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f4b10ab5ed434d1f13104f66ad35ab",
+    "url": "/agencia-ego-peojectTest/index.html"
+  },
+  {
+    "revision": "b427a80ab01d98dc08e9",
+    "url": "/agencia-ego-peojectTest/static/css/main.01163303.chunk.css"
+  },
+  {
+    "revision": "128e08f0e26c72b6be68",
+    "url": "/agencia-ego-peojectTest/static/js/2.7f509939.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/agencia-ego-peojectTest/static/js/2.7f509939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b427a80ab01d98dc08e9",
+    "url": "/agencia-ego-peojectTest/static/js/main.1420ad28.chunk.js"
+  },
+  {
+    "revision": "9fb3a4c564cb5f81088a",
+    "url": "/agencia-ego-peojectTest/static/js/runtime-main.a5c2c34e.js"
+  },
+  {
+    "revision": "68ba4f76a41711e252ea6764e2141b46",
+    "url": "/agencia-ego-peojectTest/static/media/1.68ba4f76.png"
+  },
+  {
+    "revision": "04e0dfb65acf6c544b4e1feed17b6a11",
+    "url": "/agencia-ego-peojectTest/static/media/3.04e0dfb6.png"
+  },
+  {
+    "revision": "4f04fbc3cd00b96321a2665192734b0e",
+    "url": "/agencia-ego-peojectTest/static/media/camry.4f04fbc3.png"
+  },
+  {
+    "revision": "946cd6644e494fff2134a05ae58d01d8",
+    "url": "/agencia-ego-peojectTest/static/media/corolla.946cd664.png"
+  },
+  {
+    "revision": "aa77d96e9c4afac8a211e9832c392cae",
+    "url": "/agencia-ego-peojectTest/static/media/cruiser.aa77d96e.png"
+  },
+  {
+    "revision": "e80ded57d7b5731dafc3315e36ab2414",
+    "url": "/agencia-ego-peojectTest/static/media/etios.e80ded57.png"
+  },
+  {
+    "revision": "1c9295a521c71de83595bfce598801d0",
+    "url": "/agencia-ego-peojectTest/static/media/hilux.1c9295a5.png"
+  },
+  {
+    "revision": "22650e7c87602d08665644925ba9a8f8",
+    "url": "/agencia-ego-peojectTest/static/media/img.22650e7c.png"
+  },
+  {
+    "revision": "1fe33667493fac700bb07915f4e5a043",
+    "url": "/agencia-ego-peojectTest/static/media/innova.1fe33667.png"
+  },
+  {
+    "revision": "1e993ba5dc8441a22242a60f37f57f23",
+    "url": "/agencia-ego-peojectTest/static/media/prado.1e993ba5.png"
+  },
+  {
+    "revision": "539011318d1962c2dd0bdaf02449109a",
+    "url": "/agencia-ego-peojectTest/static/media/prius-1.53901131.png"
+  },
+  {
+    "revision": "417e6fa3180fd16942295ef8e5c45470",
+    "url": "/agencia-ego-peojectTest/static/media/rav-4.417e6fa3.png"
+  },
+  {
+    "revision": "9c5b70873ee84a15b09f1018fb893925",
+    "url": "/agencia-ego-peojectTest/static/media/sw-4.9c5b7087.png"
+  },
+  {
+    "revision": "fff893863c9bc916857e2a5fd253375f",
+    "url": "/agencia-ego-peojectTest/static/media/toyota-86.fff89386.png"
+  },
+  {
+    "revision": "c0138cb3375429b7ff44a6031ee2dad5",
+    "url": "/agencia-ego-peojectTest/static/media/yaris.c0138cb3.png"
+  }
+]);
